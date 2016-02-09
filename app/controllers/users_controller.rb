df4482784f0db3e8b9de7e202aa7a3ddd4e34031
reikashiki <audrey.fa.audrey@gmail.com>
2016-02-09 06:24:15 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  #>>ここを追加しました by suzuki
+  before_action :set_user, only: [:edit, :update]
+  #<<ここを追加しました by suzuki
+
   def show 
    @user = User.find(params[:id])
   end
@@ -6,10 +10,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
 
-
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -19,12 +21,31 @@ class UsersController < ApplicationController
     end
   end
 
-
-
+  def edit
+  end
+ 
+  def update
+  #<<ここを追加しました by suzuki
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to user_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  #<<ここを追加しました by suzuki
+  end
+ 
   private
+  
+  #>>ここを追加しました by suzuki
+  def set_user
+    @user = User.find(params[:id])
+  end
+  #<<ここを追加しました by suzuki
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :profile, :location, :password,
                                  :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
-
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  #get    'edit'  , to: 'users#edit'
+  #put    'update', to: 'users#update'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
